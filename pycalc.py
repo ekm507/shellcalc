ad69ce1d94429f28a2b83befbd854ec39d949a9d
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-06-15 01:33:40 +0430

    get plot x dimention from args

--- pycalc.py
+++ pycalc.py
@@ -5,8 +5,23 @@
 # to get input from shell
 import sys
 
-# get input from shell as a string. if multiple inputs are provided, join them.
-question = ''.join(sys.argv[1:])
+
+# get input from shell as a string.
+question = sys.argv[1]
+
+# other arguments
+# get settings from shell
+x_start = -10
+x_end = 10
+x_step = 0.1
+other_args = sys.argv[2:]
+for i, arg in enumerate(other_args):
+    if arg == 'from':
+        x_start = float(other_args[i+1])
+    elif arg == 'to':
+        x_end = float(other_args[i+1])
+    elif arg == 'step':
+        x_step = float(other_args[i+1])
 
 # remove all whitespaces from input.
 # this is done to check if this is in form of 'y=f(x)' or not
@@ -25,7 +40,7 @@ if freeQ[:2] == 'y=':
     from matplotlib import pyplot as plt
     # generate x dimention of the function.
     # TODO: make it more smart or let user provide it
-    x = arange(-10, 10, 0.1)
+    x = arange(x_start, x_end, x_step)
     # execute provided command. so as an output y should be f(x)
     exec(freeQ)
     # create a plot for generated function array
